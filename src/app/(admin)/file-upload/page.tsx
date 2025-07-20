d4996147974b7b7c8fd9834cfefba25122819735
ougrid <ougridd@gmail.com>
2025-07-21 01:01:43 +0700

    feat: update export options text to include Excel format in FileUploadPage

--- src/app/(admin)/file-upload/page.tsx
+++ src/app/(admin)/file-upload/page.tsx
@@ -86,7 +86,7 @@ const FileUploadPage: React.FC = () => {
                 <svg className="w-4 h-4 text-blue-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
                 </svg>
-                Export to CSV or JSON
+                Export as Excel, CSV, or JSON
               </li>
               <li className="flex items-center">
                 <svg className="w-4 h-4 text-blue-500 mr-2" fill="currentColor" viewBox="0 0 20 20">
